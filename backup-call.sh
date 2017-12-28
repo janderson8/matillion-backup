847fbd9af9abfb5c69899d854f139c08cd32f16e
janderson8 <andersonjamesg9@gmail.com>
2017-12-28 22:17:42 +0000

    Remove backup-call shell script

--- backup-call.sh
+++
@@ -1,2 +0,0 @@
-curl -o MatillionMarketplace.json -X GET -u ec2-user:i-08d11e2ba4c67cf95 -H "Content-Type: application/json" "http://34.215.119.78/rest/v0/projects?export=false"
-curl -o Backup.json -X GET -u ec2-user:i-08d11e2ba4c67cf95 -H "Content-Type: application/json" "http://34.215.119.78/rest/v0/projects?groupName=Slalom%20Testing&projectName=JGA_Slalom_TEsting&versionName=default&export=true"
